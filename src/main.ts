40a9872a7e8cea017bc7e38fac440184d86be277
TarasIT <taras.maltsev@gmail.com>
2023-06-04 10:29:35 +0300

    start is updated

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,15 @@ import { AppModule } from './app.module';
 
 const start = async () => {
   try {
-    const PORT = process.env.PORT || 4000;
     const app = await NestFactory.create(AppModule);
     app.enableCors();
+    const PORT = process.env.PORT || 4000;
     await app.listen(PORT, () =>
       console.log(`Server is listening on port ${PORT}`),
     );
   } catch (e) {
     console.error(e);
+    process.exit(1);
   }
 };
 start();
