c984bbeaed6a9379583da2b18618b0beba56ce7a
TarasIT <taras.maltsev@gmail.com>
2023-06-05 14:31:09 +0300

    file service is updated

--- src/file/file.service.ts
+++ src/file/file.service.ts
@@ -17,7 +17,7 @@ export class FileService {
       const fileName = uuid.v4() + '.' + fileExtension;
       const filePath = path.resolve(__dirname, '..', 'static', type);
 
-      fs.mkdirSync(filePath, { recursive: true });
+      if (!fs.existsSync(filePath)) fs.mkdirSync(filePath, { recursive: true });
 
       fs.writeFileSync(path.resolve(filePath, fileName), file.buffer);
       return type + '/' + fileName;
